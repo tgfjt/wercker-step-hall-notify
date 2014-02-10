b8a5f5a5ecc4e1dc93cfb8358b4f4b8228832cd8
tgfjt <fujita@tagpanda.co.jp>
2014-02-10 22:52:38 +0900

    fix curl

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 
 curl -X POST \
      -H "Content-Type: application/json" \
-     -d "title=$WERCKER_APPLICATION_NAME" \
-     -d "message=<p>$WERCKER_RESULT</p> \
-     Build:<a href=\"$WERCKER_BUILD_URL\"" \
+     -d '{"title": "'${WERCKER_APPLICATION_NAME}'", "message": "Build:<a href=\"'${WERCKER_BUILD_URL}'\">'${WERCKER_BUILD_URL}'</a> <strong>'${WERCKER_RESULT}'</strong>"}' \
      https://hall.com/api/1/services/generic/$HALL_ROOM_API_TOKEN
