c768b39b92cf075cd8cf3e7d16a45d4197cce82a
tgfjt <fujita@tagpanda.co.jp>
2014-02-10 23:09:08 +0900

    outputlog

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
+if [ "$DEPLOY" = "true" ]; then
+  ACTION="deploy"
+  ACTION_ID=$WERCKER_DEPLOY_ID
+  ACTION_URL=$WERCKER_DEPLOY_URL
+else
+  ACTION="build"
+  ACTION_ID=$WERCKER_BUILD_ID
+  ACTION_URL=$WERCKER_BUILD_URL
+fi
+
+echo ACTION:$ACTION
+echo ACTION_ID:$ACTION_ID
+echo ACTION_URL:$ACTION_URL
+
 curl -X POST \
      -H "Content-Type: application/json" \
-     -d '{"title": "'${WERCKER_APPLICATION_NAME}'", "message": "Build:<a href=\"'${WERCKER_BUILD_URL}'\">'${WERCKER_BUILD_URL}'</a> <strong>'${WERCKER_RESULT}'</strong>"}' \
-     https://hall.com/api/1/services/generic/$HALL_ROOM_API_TOKEN
+     -d '{"title": "'${WERCKER_APPLICATION_NAME}'", "message": "Build:<a href=\"'${ACTION_URL}'\">'${ACTION_URL}'</a> <strong>'${WERCKER_RESULT}'</strong>"}' https://hall.com/api/1/services/generic/$HALL_ROOM_API_TOKEN
