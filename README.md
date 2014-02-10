062a3e28ae852c1555e2a688431cf9d47f6dd4b7
tgfjt <fujita@tagpanda.co.jp>
2014-02-10 23:26:01 +0900

    outputlog

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ POST request to Hall(.com)
 
 ```
   after-steps:
-      - tgfjt/hall-notify@0.1.1:
+      - tgfjt/hall-notify@0.1.2:
           room_token: HALL_ROOM_API_TOKEN
 ```
 
--- run.sh
+++ run.sh
@@ -14,6 +14,9 @@ echo ACTION:$ACTION
 echo ACTION_ID:$ACTION_ID
 echo ACTION_URL:$ACTION_URL
 
+echo WERCKER_APPLICATION_NAME:$WERCKER_APPLICATION_NAME
+echo WERCKER_RESULT:$WERCKER_RESULT
+
 curl -X POST \
      -H "Content-Type: application/json" \
      -d '{"title": "'${WERCKER_APPLICATION_NAME}'", "message": "Build:<a href=\"'${ACTION_URL}'\">'${ACTION_URL}'</a> <strong>'${WERCKER_RESULT}'</strong>"}' https://hall.com/api/1/services/generic/$HALL_ROOM_API_TOKEN
