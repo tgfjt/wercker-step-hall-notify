fd19a9e0e3b5cd6bdc47fef1cd052bbe16b8e1ef
tgfjt <fujita@tagpanda.co.jp>
2014-02-11 00:02:10 +0900

    add token

--- run.sh
+++ run.sh
@@ -14,8 +14,8 @@ echo ACTION:$ACTION
 echo ACTION_ID:$ACTION_ID
 echo ACTION_URL:$ACTION_URL
 
-echo $room_token
+echo $WERCKER_HALL_NOTIFY_TOKEN
 
 curl -X POST \
      -H "Content-Type: application/json" \
-     -d '{"title": "'${WERCKER_APPLICATION_NAME}'", "message": "Build:<a href=\"'${ACTION_URL}'\">'${ACTION_URL}'</a> <strong>'${WERCKER_RESULT}'</strong>"}' https://hall.com/api/1/services/generic/$room_token
+     -d '{"title": "'${WERCKER_APPLICATION_NAME}'", "message": "Build:<a href=\"'${ACTION_URL}'\">'${ACTION_URL}'</a> <strong>'${WERCKER_RESULT}'</strong>"}' https://hall.com/api/1/services/generic/$WERCKER_HALL_NOTIFY_TOKEN
